0d10933806ae76341e8b3e5ee15b3699d2cb034a
Saul Leyva Gonzalez <saul.leyva@agilethought.com>
2026-07-08 23:01:07 -0600

    window maximize

--- eclipse-workspace/selenium-training/src/test/java/com/saulito/selenium/tests/AutomationPracticeTest.java
+++ eclipse-workspace/selenium-training/src/test/java/com/saulito/selenium/tests/AutomationPracticeTest.java
@@ -14,6 +14,7 @@ public class AutomationPracticeTest {
 	@BeforeMethod
 	public void setUp() {
 		driver = new ChromeDriver();
+		//maximizar la ventana
 		driver.manage().window().maximize();
 	}
 	
